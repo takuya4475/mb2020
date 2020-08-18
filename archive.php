01236d40b06b1cd7afc13a068db5fe6c0562ddc3
yoshinaga takuya <takuya4475@gmail.com>
2020-08-18 15:42:28 +0900

    archive.php共通化、pagenaviを追加

--- archive.php
+++ archive.php
@@ -1,18 +1,6 @@
-<?php get_header(); ?>
-<div class="mv ly-bigbg">
-    <div class="ly-py60">
-      <div class="mv-container">
-        <div class="mv-flex">
-          <div class="mv-ttl-container">
-            <h2><img src="<?php echo get_template_directory_uri(); ?>/img/ttl-text.png" alt="世界最長の叙情詩をピーター・ブルック以来の全編舞台化"></h2>
-            <h1><img src="<?php echo get_template_directory_uri(); ?>/img/mv-title.png" alt="完全版マハーバーラタ"></h1>
-          </div><!-- /.mv-title-container -->
-          <p class="mv-txt_date">2020年7月4日〜7日<br>なかのZERO大ホール</p>
-        </div><!-- /.mv-flex -->
-        <button class="mv-btn btn"><a href="#">チケット予約サイトへ</a></button>
-      </div><!-- /.mv-container -->
-    </div><!-- /.ly-py-60 -->
-  </div><!-- /.mv -->
+<?php get_header(); 
+  get_template_part('sub-header');
+?>
 
   <!-- article news -->
   <article class="article-news ly-my60
@@ -29,7 +17,8 @@
           </div><!-- /.ly-pa15 -->
         </a>
       </li><!-- /.news-info -->
-<?php endwhile; endif; ?>
+      <?php endwhile; endif; ?>
     </ul><!-- /.ly-flex -->
+    <?php wp_pagenavi(); ?>
   </article><!-- /.article-news -->
 <?php get_footer(); ?>
