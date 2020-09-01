6affa4cadb5238151db57855b01e7388eb48ec0f
yoshinaga takuya <takuya4475@gmail.com>
2020-09-01 10:52:36 +0900

    story部分完成

--- page-story.php
+++ page-story.php
@@ -1,30 +1,73 @@
 <?php get_header(); ?>
 <div class="story-bg">
-  <div class="story">
-    <div class="ly-py60">
-      <div class="mv-container">
-        <div class="mv-flex">
-          <div class="mv-ttl-container">
-            <h2><img src="<?php echo get_template_directory_uri(); ?>/img/ttl-text.png" alt="世界最長の叙情詩をピーター・ブルック以来の全編舞台化"></h2>
-            <h1><img src="<?php echo get_template_directory_uri(); ?>/img/mv-title.png" alt="完全版マハーバーラタ"></h1>
-          </div><!-- /.mv-title-container -->
-          <p class="mv-txt_date">2020年7月4日〜7日<br>なかのZERO大ホール</p>
-        </div><!-- /.mv-flex -->
+  <figure class="top-img">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/story1.png" alt="">
+  </figure>
+  <div class="ly-py60">
+    <div class="mv-container">
+      <div class="mv-flex">
+        <div class="mv-ttl-container">
+          <h2><img src="<?php echo get_template_directory_uri(); ?>/img/ttl-text.png" alt="世界最長の叙情詩をピーター・ブルック以来の全編舞台化"></h2>
+          <h1><img src="<?php echo get_template_directory_uri(); ?>/img/mv-title.png" alt="完全版マハーバーラタ"></h1>
+        </div><!-- /.mv-title-container -->
+        <p class="mv-txt_date">2020年7月4日〜7日<br>なかのZERO大ホール</p>
+      </div><!-- /.mv-flex -->
       <button class="mv-btn btn"><a href="#">チケット予約サイトへ</a></button>
     </div><!-- /.mv-container -->
   </div><!-- /.ly-py-60 -->
   <!-- パンクズリスト -->
-  <div class="ly-py60 dis-ab-zIndex">
+  <div class="ly-my15 dis-ab-zIndex">
 
     <div class="breadcrumbs ly-wrapper white" typeof="BreadcrumbList" vocab="http://schema.org/">
     <?php if(function_exists('bcn_display'))
   {
     bcn_display();
   }?>
-  </div>
-</div>
+    </div><!-- /.breadcrumbs ly-wrapper white -->
+  </div><!--/.ly-my15 dis-ab-zIndex -->
+  <h2 class="story-ttl js-fadein">STORY</h2>
+  <div class="ly-wrapper">
+    <div class="story-container js-fadein">
+      <p class="story-txt">マハー（偉大な）、バーラタ（バラタ族）、つまり「偉大なバラタ族」の物語。神々が人間界を作り出すところから始まり、バラタ族の中のクル家とパーンドゥ家という二つの部族間の対立を巡るストーリー。神の血を引く個性豊かな登場人物たちによる差別、対立、欲望、嫉妬など、生の苦しみが描かれ、最終的には一族が破滅していく。<br>
+      世界的な文学作品古代ギリシャの「イーリアス」「オデュッセイア」と並ぶ世界三大叙事詩の一つに数えられており、「ラーマーヤナ」と双璧を成すインド二大叙事詩の一つ。原語はサンスクリット語であるが、全18巻、10万詩節を超えるその長さは聖書の約4倍と言われ、原典の日本語訳はいまだ完結していない。</p>
+    </div><!-- /.story-container -->
+  </div><!-- /.ly-wrapper -->
 </div><!-- /.story-bg -->
-    
 
-</div><!-- /.mv -->
-  
+<!-- 2枚目部分 -->
+<div class="story-bg">
+  <div class="story-flex_relative">
+    <figure class="story-img_left">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/story2.png" alt="">
+    </figure>
+    <div class="story-txt_right">
+        <p class="story-txt">マハー（偉大な）、バーラタ（バラタ族）、つまり「偉大なバラタ族」の物語。神々が人間界を作り出すところから始まり、バラタ族の中のクル家とパーンドゥ家という二つの部族間の対立を巡るストーリー。神の血を引く個性豊かな登場人物たちによる差別、対立、欲望、嫉妬など、生の苦しみが描かれ、最終的には一族が破滅していく。<br>
+        世界的な文学作品古代ギリシャの「イーリアス」「オデュッセイア」と並ぶ世界三大叙事詩の一つに数えられており、「ラーマーヤナ」と双璧を成すインド二大叙事詩の一つ。原語はサンスクリット語であるが、全18巻、10万詩節を超えるその長さは聖書の約4倍と言われ、原典の日本語訳はいまだ完結していない。</p>
+    </div><!-- /.ly-flex -->
+  </div><!-- /.ly-wrapper -->
+</div><!-- /.story-bg -->
+<!-- 3枚目部分 -->
+<div class="story-bg">
+  <div class="story-flex_relative row-reverse">
+    <figure class="story-img_left">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/story1.png" alt="">
+    </figure>
+    <div class="story-txt_left">
+        <p class="story-txt">マハー（偉大な）、バーラタ（バラタ族）、つまり「偉大なバラタ族」の物語。神々が人間界を作り出すところから始まり、バラタ族の中のクル家とパーンドゥ家という二つの部族間の対立を巡るストーリー。神の血を引く個性豊かな登場人物たちによる差別、対立、欲望、嫉妬など、生の苦しみが描かれ、最終的には一族が破滅していく。<br>
+        世界的な文学作品古代ギリシャの「イーリアス」「オデュッセイア」と並ぶ世界三大叙事詩の一つに数えられており、「ラーマーヤナ」と双璧を成すインド二大叙事詩の一つ。原語はサンスクリット語であるが、全18巻、10万詩節を超えるその長さは聖書の約4倍と言われ、原典の日本語訳はいまだ完結していない。</p>
+    </div><!-- /.ly-flex -->
+  </div><!-- /.ly-wrapper -->
+</div><!-- /.story-bg -->
+<!-- 2枚目部分 -->
+<div class="story-bg">
+  <div class="story-flex_relative">
+    <figure class="story-img_left">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/story2.png" alt="">
+    </figure>
+    <div class="story-txt_right">
+        <p class="story-txt">マハー（偉大な）、バーラタ（バラタ族）、つまり「偉大なバラタ族」の物語。神々が人間界を作り出すところから始まり、バラタ族の中のクル家とパーンドゥ家という二つの部族間の対立を巡るストーリー。神の血を引く個性豊かな登場人物たちによる差別、対立、欲望、嫉妬など、生の苦しみが描かれ、最終的には一族が破滅していく。<br>
+        世界的な文学作品古代ギリシャの「イーリアス」「オデュッセイア」と並ぶ世界三大叙事詩の一つに数えられており、「ラーマーヤナ」と双璧を成すインド二大叙事詩の一つ。原語はサンスクリット語であるが、全18巻、10万詩節を超えるその長さは聖書の約4倍と言われ、原典の日本語訳はいまだ完結していない。</p>
+    </div><!-- /.ly-flex -->
+  </div><!-- /.ly-wrapper -->
+</div><!-- /.story-bg -->
+<?php get_footer(); ?>
